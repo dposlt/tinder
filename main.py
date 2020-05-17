33e15fe83f10a4ae111840b45855c1159dad90fc
dposlt <david.poslt@gmail.com>
2020-05-17 13:38:44 +0200

    try more options is exists

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ __autohor__ = 'dposlt'
 __email__ = 'david.poslt@gmail.com'
 __licence__ = 'GPU'
 
-import configparser, os
+import configparser, os, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -37,9 +37,24 @@ def tinderPage():
 
     web,user,passwd, driver = readIni()
     browser = webdriver.Firefox(executable_path=checkFile())
-    browser.get(web)
 
-    login = browser.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/span/div[2]/button')
-    login.click()
+    if browser:
+        browser.get(web)
+        time.sleep(5)
+
+        ## login menu via fcb##
+
+        try:
+            browser.find_element_by_css_selector('button.Td\(u\):nth-child(3)').click()
+        except:
+            login = browser.find_element_by_css_selector('div.My\(10px\):nth-child(2) > button:nth-child(1)')
+            login.click()
+
+
+        ## Log In ##
+        email = browser.find_element_by_css_selector('#email')
+        email.text(user)
+        password = browser.find_element_by_css_selector('#pass')
+        password.text(passwd)
 
 tinderPage()
