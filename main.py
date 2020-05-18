c1948a9dd8cc08fdc35ca9a0d3699d1d1530314d
dposlt <david.poslt@gmail.com>
2020-05-18 20:02:31 +0200

    login done

--- main.py
+++ main.py
@@ -51,23 +51,43 @@ class TinderBot():
         elif self.driver.find_element_by_xpath('html/body/div[2]/div/div/div/div/div[2]/span/div[2]/button').is_displayed():
             self.driver.find_element_by_xpath('html/body/div[2]/div/div/div/div/div[2]/span/div[2]/button').click()
 
-        elif self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/span/button').is_displayed():
+        elif self.driver.find_element_by_css_selector('button.Td\(u\):nth-child(3)').is_displayed():
             self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/span/button').click()
 
 
 
 
         ## switch to login popup ##
+        time.sleep(4)
         base_window = self.driver.window_handles[1]
         self.driver.switch_to.window(base_window)
         mail = self.driver.find_element_by_xpath('//*[@id="email"]')
-        email.send_keys(user)
+        mail.send_keys(user)
 
         pwd = self.driver.find_element_by_xpath('//*[@id="pass"]')
         pwd.send_keys(passwd)
 
+        # click to login
+        if self.driver.find_element_by_xpath('//*[@id="u_0_0"]').is_displayed():
+            self.driver.find_element_by_xpath('//*[@id="u_0_0"]').click()
 
+#/html/body/div[2]/div/div/div/div/div[3]/span/button
+#/html/body/div[2]/div/div/div/div/div[3]/span/div[2]/button -- tel
+#div.My\(10px\):nth-child(2) > button:nth-child(1) -- tel
+
+    def choise(self):
+
+        ## change window ##
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to.window(base_window)
+        time.sleep(2)
+        # allow location and notification #
+        if self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]/span').is_displayed():
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]/span').click()
+        if self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]').is_displayed():
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]').click()
 
 bot = TinderBot()
 
 bot.login()
+bot.choise()
